07f43f08273b707f7bc6408012273c61ac2835d5
Jaspreet <js724415@gmail.com>
2024-03-24 23:53:03 -0700

    displayed colors

--- webappfe/src/Components/ColorList.jsx
+++ webappfe/src/Components/ColorList.jsx
@@ -9,12 +9,13 @@ const ColorList = () => {
   useEffect(() => {
     const fetchColors = async () => {
       try {
-        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImphczMiLCJyb2xlIjoicGFpbnRlciIsImlhdCI6MTcxMTM0NzM5Mn0.ZLtSDlEjIhMXxlJ3zxnYlqAYfbCudftOwWAqw0xO9aU'; // Retrieve token from storage
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImphczMiLCJyb2xlIjoicGFpbnRlciIsImlhdCI6MTcxMTM0OTIyOH0.XidJy-xegzD5ZyOiVTc5JxlYh-IhWLo2vAO3vJ8u6Qc'; 
         const response = await axios.get('http://localhost:3030/inventory', {
           headers: {
             Authorization: `Bearer ${token}` // Include token in the request headers
           }
         });
+        console.log(response.data);
         setColors(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -25,13 +26,14 @@ const ColorList = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Colors</h1>
-      {colors.map(color => (
-        <ColorInfo key={color.id} {color.color} />
-      ))}
-    </div>
-  );
-};
-
-export default ColorList;
+    
+        <div>
+          <h1>Color List</h1>
+          {colors.map((color, index) => (
+            <ColorInfo key={index} {...color} />
+          ))}
+        </div>
+      );
+    };
+    
+    export default ColorList;
